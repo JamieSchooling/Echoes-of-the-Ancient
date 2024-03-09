91ad2368c011a435793f0d194f7de6f5ac6ee937
JamieSchooling <99397650+JamieSchooling@users.noreply.github.com>
2024-03-09 04:58:14 +0000

    feat: Create Input Reader

---
+++ Assets/Scripts/Input/InputReader.cs
@@ -0,0 +1,68 @@
+using System;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+[CreateAssetMenu(menuName = "Scriptable Objects/Input/Input Reader")]
+public class InputReader : ScriptableObject, GameInput.IPlayerActions
+{
+    public event Action<Vector2> OnInputMove;
+    public event Action<Vector2> OnInputLook;
+    public event Action OnInputJump;
+
+    private GameInput _gameInput;
+    
+    private void OnEnable()
+    {
+        _gameInput ??= new GameInput();
+
+        _gameInput.Player.SetCallbacks(this);
+    }
+
+    public void OnMove(InputAction.CallbackContext context)
+    {
+        OnInputMove?.Invoke(context.ReadValue<Vector2>());
+    }
+
+    public void OnLook(InputAction.CallbackContext context)
+    {
+        OnInputLook?.Invoke(context.ReadValue<Vector2>());
+    }
+
+    public void OnJump(InputAction.CallbackContext context)
+    {
+        if (context.phase == InputActionPhase.Performed) OnInputJump?.Invoke();
+    }
+
+    public void EnableAll()
+    {
+        EnablePlayer();
+    }
+
+    public void DisableAll()
+    {
+        DisablePlayer();
+    }
+
+    public void EnablePlayer()
+    {
+        DisableCursor();
+        _gameInput.Player.Enable();
+    }
+    
+    public void DisablePlayer()
+    {
+        _gameInput.Player.Enable();
+    }
+
+    public void EnableCursor()
+    {
+        Cursor.visible = true;
+        Cursor.lockState = CursorLockMode.Confined;
+    }
+
+    public void DisableCursor()
+    {
+        Cursor.visible = false;
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+}
